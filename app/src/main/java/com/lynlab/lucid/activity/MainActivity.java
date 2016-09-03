12a1b56cdf4700bd818e3c0709bbcfc889ada85f
Do Hoerin <hellodhlyn@gmail.com>
2016-09-03 20:43:51 +0900

    앱 목록 불러오기 기능 추가

--- app/src/main/java/com/lynlab/lucid/activity/MainActivity.java
+++ app/src/main/java/com/lynlab/lucid/activity/MainActivity.java
@@ -3,6 +3,7 @@ package com.lynlab.lucid.activity;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -10,10 +11,10 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 
 import com.lynlab.lucid.R;
+import com.lynlab.lucid.fragment.ApplicationListFragment;
 
 /**
  * @author lyn
@@ -46,6 +47,10 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.activity_main_area_content, new ApplicationListFragment()).commit();
+        }
     }
 
     @Override
@@ -58,47 +63,21 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
+        Fragment fragment;
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        switch (item.getItemId()) {
+            case R.id.nav_application:
+                fragment = new ApplicationListFragment();
+                break;
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+            default:
+                return false;
         }
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.activity_main_area_content, fragment).commit();
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
---
+++ app/src/main/java/com/lynlab/lucid/fragment/ApplicationListFragment.java
@@ -0,0 +1,119 @@
+package com.lynlab.lucid.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.lynlab.lucid.R;
+import com.lynlab.lucid.api.LucidApiManager;
+import com.lynlab.lucid.model.Application;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+/**
+ * @author lyn
+ * @since 2016/09/03
+ */
+public class ApplicationListFragment extends Fragment {
+
+    private ApplicationAdapter adapter;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_application_list, container, false);
+
+        adapter = new ApplicationAdapter();
+
+        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.fragment_application_list_recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(adapter);
+
+        requestData();
+
+        return view;
+    }
+
+    /**
+     * 애플리케이션 데이터를 불러온다.
+     */
+    private void requestData() {
+        LucidApiManager apiManager = LucidApiManager.getInstance(getContext());
+
+        Call<List<Application>> call = apiManager.getService().getApplications();
+        call.enqueue(new Callback<List<Application>>() {
+            @Override
+            public void onResponse(Call<List<Application>> call, Response<List<Application>> response) {
+                if (response.isSuccessful()) {
+                    adapter.setApplications(response.body());
+                    adapter.notifyDataSetChanged();
+                } else {
+
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Application>> call, Throwable t) {
+
+            }
+        });
+    }
+
+
+    private class ApplicationAdapter extends RecyclerView.Adapter<ApplicationViewHolder> {
+
+        private List<Application> applications;
+
+        public ApplicationAdapter() {
+            applications = new ArrayList<>();
+        }
+
+        @Override
+        public ApplicationViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(getContext()).inflate(R.layout.item_application, parent, false);
+            return new ApplicationViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(ApplicationViewHolder holder, int position) {
+            holder.bind(applications.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return applications.size();
+        }
+
+        public void setApplications(List<Application> applications) {
+            this.applications = applications;
+        }
+    }
+
+    private class ApplicationViewHolder extends RecyclerView.ViewHolder {
+
+        TextView nameTextView;
+        TextView packageTextView;
+
+        public ApplicationViewHolder(View itemView) {
+            super(itemView);
+            nameTextView = (TextView) itemView.findViewById(R.id.item_application_name);
+            packageTextView = (TextView) itemView.findViewById(R.id.item_application_package);
+        }
+
+        public void bind(Application application) {
+            nameTextView.setText(application.getName());
+            packageTextView.setText(application.getPackageName());
+        }
+    }
+}
